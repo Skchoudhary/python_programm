08415f88e1a29e41ca36f24fd72a1653c9003f45
Mrsk <choudharysaab786@gmail.com>
2016-07-23 20:02:33 +0530

    list as stack adn queue

---
+++ list_as_queue.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+a=[1,2,3,4,5]
+print("list :", a)
+
+a.append(21)
+print("new list : ", a)
+
+a.pop(0)
+print("new list :", a)
+
+
+
---
+++ list_as_stack.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+a=[1, 4, 3, 53, 2]
+print("list :", a)
+
+print(a.pop()) #poping last element
+print("pop element from list :", a)
+
+a.append(21) #adding element in list
+print("adding element in list :", a)
